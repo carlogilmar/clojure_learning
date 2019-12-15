197f9ca19d7c322816d3309402e13d26a663e209
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-15 17:32:08 -0600

    Adding arity 1 for my function

--- fizzbuzz.clj
+++ fizzbuzz.clj
@@ -1,11 +1,21 @@
-(defn fizzbuzz [i]
-  (cond
-    (= 0 (mod i (* 3 5)) "FizzBuzz"
-    (= 0 (mod i 5)) "Fizz"
-    (= 0 (mod i 3)) "Buzz"
-    :else (str i)))
+;(defn fizzbuzz [i]
+;  (cond
+;    (= 0 (mod i (* 3 5)) "FizzBuzz"
+;    (= 0 (mod i 5)) "Fizz"
+;    (= 0 (mod i 3)) "Buzz"
+;    :else (str i))))
 
-(fizzbuzz 1)
+(defn fizzbuzz
+  ([index]
+  (cond
+    (= 0 (mod index (* 3 5))) (println "FizzBuzz")
+    (= 0 (mod index 5)) (println "Fizz")
+    (= 0 (mod index 3)) (println "Buzz")
+    :else (println index)
+    ))
+  )
+
+(fizzbuzz 15)
 (fizzbuzz 5)
-(fizzbuzz 6)
+(fizzbuzz 3)
 (fizzbuzz 3)
